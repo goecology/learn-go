004914b0812314a49e31b28b6ddaeae9366a4d2a
yitume-saber <yitume@163.com>
2020-02-19 21:34:02 +0800

    init lesson1 part1

---
+++ advanced/lesson1/part1/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/goecology/muses"
+	"github.com/goecology/muses/pkg/cmd"
+	"github.com/spf13/cobra"
+)
+
+var HelloCmd = &cobra.Command{
+	Use:  "hello",
+	Long: `Show hello information`,
+	Run:  helloCmd,
+}
+
+func helloCmd(cmd *cobra.Command, args []string) {
+	fmt.Println("hello goecology")
+}
+
+func main() {
+	app := muses.Container(
+		cmd.Register,
+	)
+	app.SetRootCommand(func(cobraCommand *cobra.Command) {
+		cobraCommand.AddCommand(HelloCmd)
+	})
+
+	err := app.Run()
+	if err != nil {
+		panic(err)
+	}
+}
